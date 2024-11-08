7b22c940eea7e2d404a004889bd2f4103f0b3a63
Atharv7740 <atharv7740@gmail.com>
2024-11-08 21:11:59 +0530

    Update with CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 const authRoutes = require('./src/routes/authRoutes');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -14,16 +15,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use('/api/auth', authRoutes);
+
 app.get('/api/test', (req, res) => {
   // Simulate an error
   throw new Error('Test error');
 });
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
 
-app.use('/api/auth', authRoutes);
+app.use(cors({ origin: 'http://localhost:5173', methods: ['GET', 'POST', 'OPTIONS'], allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'] }));
 
 const PORT = process.env.PORT || 5000;
 
