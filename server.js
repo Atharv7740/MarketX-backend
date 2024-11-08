24fba7dcaa820e3fbcc1ed2fbe10834c7041c5d2
Atharv7740 <atharv7740@gmail.com>
2024-11-08 15:24:55 +0530

    update

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/test', (req, res) => {
+  // Simulate an error
+  throw new Error('Test error');
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
@@ -17,3 +30,23 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.get('/api/test', (req, res) => {
+//   // Simulate an error
+//   throw new Error('Test error');
+// });
+
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
